3370cbb7fd6c3019bb24b36471281c43c9fb3fda
Hunter Haggard <hunterhaggard5@gmail.com>
2026-05-08 13:04:02 -0400

    new commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return(
+
+   <div className="flex items-center justify-center h-screen">
+    <div className="text-3xl font-bold underline"
+    >
+    Hunter Haggard
+    </div>
+   </div> 
+  )
+}
+
